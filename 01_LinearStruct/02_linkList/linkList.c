20a49e04eb4596f942996380444022107e22db14
Yy-sean <Ada773458@outlook.com>
2025-07-16 09:10:50 +0800

    add some explanational annotations

--- 01_LinearStruct/02_linkList/linkList.c
+++ 01_LinearStruct/02_linkList/linkList.c
@@ -36,6 +36,32 @@ int insertLinkListHeader(LinkList_t *link_table, Element_t val){
     return 0;
 }
 
+
+int insertLinkListPos(LinkList_t *link_table, int pos, Element_t val){
+    if(pos <= 0 || pos > link_table->count+1){//+1 是因为允许在链表末尾之后插入新节点
+        printf("Position out of range.\n");
+        return -1; // 位置超出范围
+    }
+    
+    node_t *p = &link_table->head;
+    int index = -1;// 记录插入位置的前置节点的索引,`index = -1` 的设计强制区分头节点和数据节点，减少歧义。
+
+    while(p->next != NULL && index < pos-1){
+        p = p->next;
+        index++;
+    }
+    
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    if(new_node == NULL){//在C语言中，使用malloc函数分配内存时，如果内存分配失败，malloc将返回NULL
+        printf("Memory allocation failed.\n");
+        return -1;
+    }
+    new_node->val = val;
+    new_node->next = p->next;
+    p->next = new_node;
+    link_table->count++;
+    return 0;
+}
 int deleteLinkListElement(LinkList_t *link_table, Element_t val){
     //辅助指针，指向val这个元素的前置节点
     node_t *p = &link_table->head;
